39b19a452654bcdd14b9c0e19c82ae68a3592eb7
Paweł Janeczek <pjanecze@gmail.com>
2021-09-24 16:43:31 +0200

    WIP

---
+++ src/features/configure/vault/moonriver_pools.js
@@ -0,0 +1,27 @@
+export const bscPools = [
+  {
+    id: 'moonfarm-wmovr-dai-usdc',
+    name: 'DAI-USDC LP',
+    token: 'DAI-USDC LP',
+    tokenDescription: 'Moonfarm',
+    tokenAddress: '0x07866497aAF2E8B201300759720C5Ac873DbF0e7',
+    tokenDecimals: 18,
+    tokenDescriptionUrl: '#',
+    earnedToken: 'miiMoonfarmDAI-USDC',
+    earnedTokenAddress: '0xDD7cf25F771140D16fAE79d22eF63fF055BD28d0',
+    earnContractAddress: '0xDD7cf25F771140D16fAE79d22eF63fF055BD28d0',
+    pricePerFullShare: 1,
+    tvl: 0,
+    oracle: 'lps',
+    oracleId: 'moonfarm-dai-usdc',
+    oraclePrice: 0,
+    depositsPaused: false,
+    status: 'active',
+    platform: 'Moonfarm',
+    assets: ['DAI', 'USDC'],
+    addLiquidityUrl:
+      'https://swap.moonswap.in/#/add/0x80A16016cC4A2E6a2CACA8a4a498b1699fF0f844/0xE3F5a90F9cb311505cd691a46596599aA1A0AD7D',
+    buyTokenUrl:
+      'https://swap.moonswap.in/swap'
+  }
+];
